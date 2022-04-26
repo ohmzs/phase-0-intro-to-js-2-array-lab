7600a707cf99304907f4a1a2d2db2b99675ca625
Omar <omartsadek@gmail.com>
2022-04-26 12:38:04 -0400

    commit tree 271a27fdec21052f9ec5b2cea73208c97c2455e5

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]                 //is assigned an initial value of ["Milo", "Otis", "Garfield"]
+
+    function destructivelyAppendCat(name){              //appends a cat to the end of the cats array
+                        cats.push(name);     
+}
+    function destructivelyPrependCat(name){             //prepends a cat to the beginning of the cats array
+                        cats.unshift(name);
+    }
+    function destructivelyRemoveLastCat(){
+                        cats.pop();
+    }
+    function destructivelyRemoveFirstCat(){
+                        cats.shift();
+    }
+    function appendCat(name){
+                    return [...cats, name];
+    }
+    function prependCat(name){
+                    return [name, ...cats];
+    }
+    function removeLastCat(){
+                    return cats.slice(0,2)
+    }
+    function removeFirstCat(){
+                    return cats.slice(1)
+    }
+
+//     destructivelyPrependCat(name)
+//     ✓ prepends a cat to the beginning of the cats array
+
+//   destructivelyRemoveFirstCat()
+//     ✓ removes the first cat from the cats array
+//   appendCat(name)
+//     ✓ appends a cat to the cats array and returns a new array, leaving the cats array unchanged
+//   prependCat(name)
+//     ✓ prepends a cat to the cats array and returns a new array, leaving the cats array unchanged
+//   removeLastCat()
+//     ✓ removes the last cat in the cats array and returns a new array, leaving the cats array unchanged
+//   removeFirstCat()
+//     ✓ removes the first cat from the cats array and returns a new array, leaving the cats array unchanged
